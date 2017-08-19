0ea4825797a623a67eff0f70b64f2da8c4cd5f2f
BeBruceThomas <bruce.eo.thomas@gmail.com>
2017-08-18 20:05:45 -1000

    Add files via upload

---
+++ data/dataForOkada.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Kilauea_Project
+@author: bruce.eo.thomas
+"""
+
+"""
+Script for the Okada Data
+CHANGE PATH! 
+"""
+
+
+# Moduls imported
+import numpy as np
+
+
+path = "/gps/Bruce/KilaueaKoaeProject/data/dataForOkada"
+
+
+
+okada_initial_params = np.zeros((4, 11))
+
+upper_bounds = open(path+"/upper_bounds.txt", "r")
+for i in range(11):
+    okada_initial_params[0][i] = upper_bounds.readline()
+upper_bounds.close()
+
+okada_start = open(path+"/okada_start.txt", "r")
+for i in range(11):
+    okada_initial_params[1][i] = okada_start.readline()
+okada_start.close()
+
+okada_pr = open(path+"/okada_pr.txt", "r")
+for i in range(11):
+    okada_initial_params[2][i] = okada_pr.readline()
+okada_pr.close()
+
+lower_bounds = open(path+"/lower_bounds.txt", "r")
+for i in range(11):
+    okada_initial_params[3][i] = lower_bounds.readline()
+lower_bounds.close()
+
+
+
+okada_intern = np.zeros((1, 11))
+
+okada_prI = open(path+"/okada_prI.txt", "r")
+for i in range(11):
+    okada_intern[0][i] = okada_prI.readline()
+okada_prI.close()
